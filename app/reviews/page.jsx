258623a6972ca28efa226101b32f2927f9346754
unknown <chirantannt19@gmail.com>
2026-02-10 10:25:00 +0530

    add the review page

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -14,7 +14,7 @@ const reviews = async () => {
             <div className='px-10 my-5'>
                 <div className="pt-40 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
                     {reviews?.map((rev) => (
-                        <div className=" h-70 w-110 flex flex-col mb-10 bg-charcoal/40 rounded-[2.5rem] overflow-hidden border border-white/5 hover:border-primary-500/30 transition-all duration-500">
+                        <div className="h-70 w-110 flex flex-col mb-10 bg-charcoal/40 rounded-[2.5rem] overflow-hidden border border-white/5 hover:border-primary-500/30 transition-all duration-500">
                             <h3 className='text-x py-7 text-zinc-300  px-10 font-mono font-extralight'>"{rev.comment}"</h3>
                             <div className=''>
                                 <h1 className='text-x px-10 py-2 font-bold text-white'>{rev.customer_name}</h1>
